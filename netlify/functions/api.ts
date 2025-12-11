6adf0187238fb9d8024dca466497998e58f4d3b0
Axeliriya <axeliriya@yandex.ru>
2025-12-11 13:35:22 +0200

    fix: perfect Netlify Functions handler without TS errors

---
+++ netlify/functions/api.ts
@@ -0,0 +1,11 @@
+import serverless from "serverless-http";
+import app from "../../src/app.js";
+import type { Handler, APIGatewayProxyEvent, Context } from "aws-lambda";
+
+export const handler: Handler = serverless(app, {
+  request: (request: any, event: APIGatewayProxyEvent) => {
+    request.headers = event.headers ?? {};
+    request.method = event.httpMethod;
+    request.url = event.path;
+  },
+});
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import usersRoutes from "./routes/usersRoutes";
+import usersRoutes from "./routes/usersRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use("/users", usersRoutes);
 
 export default app;
