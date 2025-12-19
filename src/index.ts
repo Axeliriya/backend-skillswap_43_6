3d1acd0a9ce08daeb965242be2174d58818637ea
Axeliriya <axeliriya@yandex.ru>
2025-12-19 03:14:30 +0200

    Fix: merge app into index.ts for Coolify deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-import app from "./app";
+import express from "express";
 import dotenv from "dotenv";
+import usersRoutes from "./routes/usersRoutes.js";
+import categoriesRoutes from "./routes/categoriesRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use("/auth", authRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/users", usersRoutes);
+
 const PORT = Number(process.env.PORT) || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+export default app;
